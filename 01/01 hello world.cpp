ea34fcd283bfed20a54835b842298418b228fa84
avhkbcj99 <>
2023-05-05 08:45:20 +0800

    first commit

---
+++ 01/01 hello world.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+const int WEEK = 7;
+# define HOURS 24
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    cout << "Hello, World!" << endl;
+    int age = 18;
+    cout << "age = " << age << endl;
+    cout << "a week have " << WEEK << " days" << endl;
+//    WEEK = 8;
+    cout << "a day have " << HOURS << " hours" << endl;
+//    HOURS++;
+    age++;
+    cout << "age = " << age << endl;
+
+    return 0;
+}
